9b0088ff733086ffe81dfa37460405a888a2c3f1
K-Pratzz <kp939331@gmail.com>
2026-03-13 05:43:54 +0530

    11

---
+++ ARRAYS/11.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l=0
+        r=len(height)-1
+        maxarea=0
+        while l<r:
+            width=r-l
+            currHeight=min(height[l],height[r])
+            area=width*currHeight
+            maxarea=max(area,maxarea)
+            if height[l]<height[r]:
+                l+=1
+            else:
+                r-=1
+        
+        return maxarea
