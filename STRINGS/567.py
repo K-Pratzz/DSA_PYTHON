367153a61cc991b593415bb96db969cbcc5d472c
K-Pratzz <kp939331@gmail.com>
2026-04-16 07:20:38 +0530

    567

---
+++ STRINGS/567.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1,n2=len(s1),len(s2)
+        if n1>n2:
+            return False
+        l1=[0]*26
+        l2=[0]*26
+        for i in range(n1):
+            l1[ord(s1[i])-ord('a')]+=1
+            l2[ord(s2[i])-ord('a')]+=1
+        if l1==l2:
+            return True
+        for i in range(n1,n2):
+            l2[ord(s2[i])-ord('a')]+=1
+            l2[ord(s2[i-n1])-ord('a')]-=1
+            if l1==l2:
+                return True
+        return False
