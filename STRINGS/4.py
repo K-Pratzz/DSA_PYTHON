edee637dd50054b78a220189f8fe7f75d3eedef6
K-Pratzz <kp939331@gmail.com>
2026-06-08 07:16:40 +0530

    4

---
+++ STRINGS/4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2) -> float:
+        nums=nums1+nums2
+        nums.sort()
+        n=len(nums)
+        mid=n//2
+        if n%2==0:
+            return (nums[mid-1]+nums[mid])/2.0
+        else:
+            return float(nums[mid])
